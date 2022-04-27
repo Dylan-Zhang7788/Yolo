7d12cf30e60198a177d4cd8c2a305cd2c29f405a
Dylan-Zhang7788 <Dylan_zhang7788@outlook.com>
2022-04-27 10:19:09 +0800

    v1.1

--- train.py
+++ train.py
@@ -19,15 +19,15 @@ canvas2 = hl.Canvas()
 stop_count=0
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-file_root_train = '/home/xcy/my_code/seg/VOC2012/JPEGImages/'
-file_root_val='/home/xcy/my_code/seg/VOC2012/JPEGImages/'
+file_root_train = '/home/xcy/zhangdi_ws/seg/VOC2012/JPEGImages/'
+file_root_val='/home/xcy/zhangdi_ws/seg/VOC2012/JPEGImages/'
 batch_size = 10
 learning_rate = 0.001
 num_epochs = 100
 
 train_dataset = yoloDataset(
     root=file_root_train, list_file=
-        '/home/xcy/my_code/seg/voc2012test.txt', train=True, transform=[
+        '/home/xcy/zhangdi_ws/seg/voc2012test.txt', train=True, transform=[
             transforms.ToTensor()])
 train_loader = DataLoader(
     train_dataset,
@@ -36,7 +36,7 @@ train_loader = DataLoader(
     num_workers=0)
 val_dataset = yoloDataset(
     root=file_root_val,
-    list_file='/home/xcy/my_code/seg/voc2012test.txt',
+    list_file='/home/xcy/zhangdi_ws/seg/voc2012test.txt',
     train=False,
     transform=[
         transforms.ToTensor()])
